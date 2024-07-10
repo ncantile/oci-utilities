89da682eb2c1cb1fb432cf454b233b484d31d494
ncantile <ncantile@sorint.com>
2024-07-10 10:07:20 +0200

    Added support for outputting the configuration

--- network-scripts/create_ipsec.py
+++ network-scripts/create_ipsec.py
@@ -485,33 +485,32 @@ else:
 tunnels = network_client.list_ip_sec_connection_tunnels(ipsc_id = ipsecOCID).data
 tunnelOCIDs = [tunnel.id for tunnel in tunnels]
 
-def getTunnelIP(ipsecOCID, tunnelOCID):
-    tunnelNamesAndIP = {tunnels[0].display_name: tunnels[0].vpn_ip, tunnels[1].display_name: tunnels[1].vpn_ip}
-
-
-
+tunnelNamesAndIP = {tunnels[0].display_name: tunnels[0].vpn_ip, tunnels[1].display_name: tunnels[1].vpn_ip}
 
 print ('Below you can find the configuration file. Please provide this file to the networking team entrusted with configuring the actual Customer-Premises equipment.')
-print ('|-------------------|--------------------------------------------------------------------------------|')
+print('|--------------------------------|-----------------------------------------------------------------------|')
 print(f'| {"CPE IP": <30} | {cpeIP: <70}|')
 print(f'| {"IKE version": <30} | {ikeVer: <70}|')
 print(f'| {"Routing Type": <30} | {routingType.replace("POLICY","STATIC"): <70}|')
 print(f'| {"Shared Secret": <30} | {tunnelSecret: <70}|')
 for tun in tunnelNames:
-    print(f'| {tun: <100}|')
-    print(f'| {"Tunnel IP endpoint": <30} | {tunnelNamesAndIP[tun]: <70}|')
-    print(f'| {"Oracle interface IP": <30} | {insideIP[tunnelIndex]: <70}|')
-    print(f'| {"CPE interface IP": <30} | {outsideIP[tunnelIndex]: <70}|')
-    print(f'| {"Phase 1 info": <30} | {" ": <70}|')
-    print(f'| {"  Authentication algorithm": <30} | {"SHA2-384": <70}|')
-    print(f'| {"  Encryption algorithm": <30} | {"AES-256-CBC": <70}|')
-    print(f'| {"  DH Group": <30} | {"group 20 (ECP 384-bit random)": <70}|')
-    print(f'| {"  IKE lifetime": <30} | {"28800 seconds (8 hours)": <70}|')
-    print(f'| {"Phase 2 info": <30} | {" ": <70}|')
-    print(f'| {"  Authentication algorithm": <30} | {"HMAC-SHA-256-128": <70}|')
-    print(f'| {"  Encryption algorithm": <30} | {"AES-256-GCM": <70}|')
-    print(f'| {"  Perfect-forward secrecy": <30} | {"Enabled": <70}|')
-    print(f'| {"  DH Group": <30} | {"group 5 (MODP 1536-bit)": <70}|')
-    print(f'| {"  IPSec lifetime": <30} | {"3600 seconds (1 hour)": <70}|')
-print ('======================================================================================================')
-
+    tunnelIndex = 0
+    print(f'| {tun: <103}|')
+    print(f'| {"  Tunnel IP endpoint": <30} | {tunnelNamesAndIP[tun]: <70}|')
+    if len(insideIP) > 0:
+        print(f'| {"  Oracle interface IP": <30} | {insideIP[tunnelIndex]: <70}|')
+    if len(outsideIP) > 0:
+        print(f'| {"  CPE interface IP": <30} | {outsideIP[tunnelIndex]: <70}|')
+    print(f'| {"  Phase 1 info": <30} | {" ": <70}|')
+    print(f'| {"    Authentication algorithm": <30} | {"SHA2-384": <70}|')
+    print(f'| {"    Encryption algorithm": <30} | {"AES-256-CBC": <70}|')
+    print(f'| {"    DH Group": <30} | {"group 20 (ECP 384-bit random)": <70}|')
+    print(f'| {"    IKE lifetime": <30} | {"28800 seconds (8 hours)": <70}|')
+    print(f'| {"  Phase 2 info": <30} | {" ": <70}|')
+    print(f'| {"    Authentication algorithm": <30} | {"HMAC-SHA-256-128": <70}|')
+    print(f'| {"    Encryption algorithm": <30} | {"AES-256-GCM": <70}|')
+    print(f'| {"    Perfect-forward secrecy": <30} | {"Enabled": <70}|')
+    print(f'| {"    DH Group": <30} | {"group 5 (MODP 1536-bit)": <70}|')
+    print(f'| {"    IPSec lifetime": <30} | {"3600 seconds (1 hour)": <70}|')
+    tunnelIndex += 1
+print ('|========================================================================================================|')
